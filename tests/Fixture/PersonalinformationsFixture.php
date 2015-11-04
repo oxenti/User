a0c8f6c8c8e074de542663da8e095949aed37b83
Adewale Andrade <adewale.andrade@gmail.com>
2015-11-04 18:52:11 -0300

    correção de fixtures

--- tests/Fixture/PersonalinformationsFixture.php
+++ tests/Fixture/PersonalinformationsFixture.php
@@ -18,7 +18,6 @@ class PersonalinformationsFixture extends AppFixture
     // @codingStandardsIgnoreStart
     public $fields = [
         'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
-        'user_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'first_name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
         'last_name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
         'birth' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
@@ -50,7 +49,6 @@ class PersonalinformationsFixture extends AppFixture
     public $records = [
         [
             'id' => 1,
-            'user_id' => 1,
             'first_name' => 'Lorem ipsum dolor sit amet',
             'last_name' => 'Lorem ipsum dolor sit amet',
             'birth' => '2015-10-07',
